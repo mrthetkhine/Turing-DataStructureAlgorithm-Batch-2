c95ddffe5ed40d9165abb4f8c0549af6f67a92a5
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2025-04-03 21:26:58 +0630

    add Dijkstra

--- src/main/java/com/turing/dsa/graph/GraphAMatrix.java
+++ src/main/java/com/turing/dsa/graph/GraphAMatrix.java
@@ -2,18 +2,41 @@ package com.turing.dsa.graph;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Stack;
 
+class VerticeDistance implements Comparable<VerticeDistance>
+{
+	String vertice;
+	double totalDistance;
+	
+	public VerticeDistance(String vertice,double totalDistance)
+	{
+		this.vertice = vertice;
+		this.totalDistance = totalDistance;
+	}
+	
+	public int compareTo(VerticeDistance other)
+	{
+		return (int)(this.totalDistance - other.totalDistance);
+	}
+
+	
+}
 public class GraphAMatrix extends Graph{
 	
 	int[][]matrix ;
 	
+	double[] weights ;
+	String[] previous;
 	
 	public void build() {
 		
 		int n = this.vertices.size();
 		this.matrix = new int[n][n];
+		this.weights = new double[n];
+		this.previous = new String[n];
 	}
 
 	public void addEdge(String from, String to) {
@@ -23,12 +46,18 @@ public class GraphAMatrix extends Graph{
 		this.matrix[fromIndex][toIndex] = 1;
 		this.matrix[toIndex][fromIndex] = 1;
 	}
-
+	public void addEdge(String from, String to,int weight) {
+		int fromIndex = this.vertices.indexOf(from);
+		int toIndex = this.vertices.indexOf(to);
+		
+		this.matrix[fromIndex][toIndex] = weight;
+		this.matrix[toIndex][fromIndex] = weight;
+	}
 	public boolean existEdge(String from, String to) {
 		int fromIndex = this.vertices.indexOf(from);
 		int toIndex = this.vertices.indexOf(to);
 		
-		return this.matrix[fromIndex][toIndex] != 0;
+		return this.matrix[fromIndex][toIndex] != 0 || this.matrix[toIndex][fromIndex] !=0;
 	}
 
 	public List<String> getAdjacentVertex(String from)
@@ -47,6 +76,101 @@ public class GraphAMatrix extends Graph{
 		}
 		return vertexs;
 	}
-	
+	double getTotalDistance(String vertice)
+	{
+		int verticeIndex = this.vertices.indexOf(vertice);
+		String previous =null; 
+		double distance =0;
+		
+		do
+		{
+			distance += this.weights[verticeIndex];
+			previous =  this.previous[verticeIndex];
+			if(previous !=null)
+			{
+				verticeIndex =this.vertices.indexOf(previous);
+			}
+		}while(previous != null);
+		return distance;	
+	}
+	VerticeDistance getVerticeWithSmallestDistanceFromUnvisited(ArrayList<String> unvisited)
+	{
+		ArrayList<VerticeDistance> verticeAndDistances = new ArrayList<>();
+		
+		for(String unvisitedVertice: unvisited)
+		{
+			double totalDistance = this.getTotalDistance(unvisitedVertice);
+			VerticeDistance verticeDistance = new VerticeDistance(unvisitedVertice,totalDistance);
+			verticeAndDistances.add(verticeDistance);
+		}
+		Collections.sort(verticeAndDistances);
+		return verticeAndDistances.get(0);
+		
+	}
+	public void initDijkstra()
+	{
+		for(int i=0;i< this.weights.length;i++)
+		{
+			this.weights[i] = Double.POSITIVE_INFINITY;
+		}
+		this.weights[0] = 0;
+	}
+	void addTotalWeightToVertice(String vertice,double weight)
+	{
+		int index = this.vertices.indexOf(vertice);
+		this.weights[index] = weight;
+	}
+	void addTotalWeightToVerticeWithPrevious(String vertice,double weight,String previous)
+	{
+		int index = this.vertices.indexOf(vertice);
+		this.weights[index] = weight;
+		this.previous[index] = previous;
+	}
+	List<String> getUnvisitedNeighbourOfVertice(String from, ArrayList<String> unvisited)
+	{
+		List<String> unvisitedNeighBour = new ArrayList<>();
+		for(String vertice: this.vertices)
+		{
+			if(unvisited.contains(vertice) && this.existEdge(from, vertice))
+			{
+				unvisitedNeighBour.add(vertice);
+			}
+		}
+		return unvisitedNeighBour;
+	}
+	//pseudocode from http://www.gitta.info/Accessibiliti/en/html/Dijkstra_learningObject1.html
+	public String[] dijkstraAlgorithm()
+	{
+		this.initDijkstra();
+		
+		ArrayList<String> unvisited = new ArrayList<>();
+		for(String vertice: this.vertices)
+		{
+			unvisited.add(vertice);
+		}
+		while(unvisited.size()>0)
+		{
+			VerticeDistance vd = this.getVerticeWithSmallestDistanceFromUnvisited(unvisited);
+			System.out.println("Smallest vertice "+vd.vertice +" with distance "+vd.totalDistance);
+			unvisited.remove(vd.vertice);
+			
+			List<String> neighbour = this.getUnvisitedNeighbourOfVertice(vd.vertice, unvisited);
+			for(String neighbourVertice : neighbour)
+			{
+				
+				int index = this.vertices.indexOf(vd.vertice);
+				int neighourIndex = this.vertices.indexOf(neighbourVertice);
+				double alt = this.weights[index] + this.matrix[index][neighourIndex];
+				
+				if(alt < this.weights[index])
+				{
+					this.weights[index] = alt;
+					this.previous[index] = neighbourVertice;
+				}
+				
+			}
+		}
+		return this.previous;
+	}
 
 }
---
+++ src/test/java/com/turing/dsa/graph/DijkstraTest.java
@@ -0,0 +1,133 @@
+package com.turing.dsa.graph;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class DijkstraTest {
+
+	GraphAMatrix graph = new GraphAMatrix();
+	DijkstraTest()
+	{
+		
+		graph.addVertice("A");
+		graph.addVertice("B");
+		graph.addVertice("C");
+		graph.addVertice("D");
+		graph.addVertice("E");
+		graph.addVertice("F");
+		graph.addVertice("G");
+		
+		graph.build();
+		
+		graph.addEdge("A", "B", 3);
+		graph.addEdge("A", "C", 5);
+		graph.addEdge("A", "D", 1);
+		
+		graph.addEdge("B", "C", 2);
+		
+		graph.addEdge("C", "D", 2);
+		graph.addEdge("C", "G", 4);
+		
+		graph.addEdge("D", "E", 1);
+		graph.addEdge("D", "F", 6);
+		
+		graph.addEdge("E", "F", 3);
+		graph.addEdge("E", "G", 3);
+		
+		
+		graph.initDijkstra();
+	}
+	//Graph from https://www.savemyexams.com/a-level/computer-science/ocr/17/revision-notes/8-algorithms/8-1-algorithms/dijkstras-shortest-path-algorithm/
+	@Test
+	public void testGetMiniDistanceVerticeFromUnvisitedNodeBaseCase()
+	{
+		
+		
+		ArrayList<String> unvisited = new ArrayList<>();
+		for(String vertice: graph.vertices)
+		{
+			unvisited.add(vertice);
+		}
+		VerticeDistance vd= graph.getVerticeWithSmallestDistanceFromUnvisited(unvisited);
+		
+		assertEquals("A",vd.vertice);
+	}
+	
+	@Test
+	public void testGetMiniDistanceCompleCase()
+	{
+		
+		ArrayList<String> unvisited = new ArrayList<>();
+		for(String vertice: graph.vertices)
+		{
+			unvisited.add(vertice);
+		}
+		unvisited.remove("A");
+		
+		graph.addTotalWeightToVertice("B", 3);
+		graph.addTotalWeightToVertice("C", 5);
+		graph.addTotalWeightToVertice("D", 1);
+		VerticeDistance vd = graph.getVerticeWithSmallestDistanceFromUnvisited(unvisited);
+		
+		assertEquals("D",vd.vertice);
+	}
+	@Test
+	public void testGetMiniDistanceCompleCaseWithPreious()
+	{
+		
+		ArrayList<String> unvisited = new ArrayList<>();
+		for(String vertice: graph.vertices)
+		{
+			unvisited.add(vertice);
+		}
+		unvisited.remove("A");
+		
+		graph.addTotalWeightToVertice("B", 7);
+		graph.addTotalWeightToVertice("C", 9);
+		graph.addTotalWeightToVerticeWithPrevious("D", 4,"A");
+		graph.addTotalWeightToVerticeWithPrevious("E", 2,"D");
+		
+		VerticeDistance vd = graph.getVerticeWithSmallestDistanceFromUnvisited(unvisited);
+		
+		assertEquals("D",vd.vertice);
+		assertEquals(4,vd.totalDistance);
+	}
+	@Test
+	public void testUnvisitedNeighbour()
+	{
+		
+		ArrayList<String> unvisited = new ArrayList<>();
+		for(String vertice: graph.vertices)
+		{
+			unvisited.add(vertice);
+		}
+		//unvisited.remove("A");
+		
+		List<String> neighbour = graph.getUnvisitedNeighbourOfVertice("A", unvisited);
+		assertTrue(neighbour.contains("B"));
+		assertTrue(neighbour.contains("C"));
+		assertTrue(neighbour.contains("D"));
+	}
+	@Test
+	public void testUnvisitedNeighbourComplexCase()
+	{
+		
+		ArrayList<String> unvisited = new ArrayList<>();
+		for(String vertice: graph.vertices)
+		{
+			unvisited.add(vertice);
+		}
+		unvisited.remove("B");
+		
+		List<String> neighbour = graph.getUnvisitedNeighbourOfVertice("A", unvisited);
+		assertFalse(neighbour.contains("B"));
+		assertTrue(neighbour.contains("C"));
+		assertTrue(neighbour.contains("D"));
+	}
+}
